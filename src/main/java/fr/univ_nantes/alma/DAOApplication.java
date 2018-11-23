e7b95ec4e7061afb0e4aaecc5b925e38ad642ad9
Martin <ars.martin@hotmail.fr>
2018-11-23 16:47:56 +0100

    modification

--- src/main/java/fr/univ_nantes/alma/DAOApplication.java
+++ src/main/java/fr/univ_nantes/alma/DAOApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import fr.univ_nantes.alma.engine.Minion;
-import fr.univ_nantes.alma.engine.Card;
-import fr.univ_nantes.alma.engine.CardRepository;
+import fr.univ_nantes.alma.engine.MinionRepository;
 
 @SpringBootApplication
 public class DAOApplication {
@@ -17,38 +16,16 @@ public class DAOApplication {
     }
     
     @Bean
-	public CommandLineRunner demo(CardRepository repository) {
+	public CommandLineRunner demo(MinionRepository repository) {
 		return (args) -> {
 			// save a couple of customers
 			repository.save(new Minion(1, "common", "chevalier", 2, 1, "target", "description", 2, false, false, false, 0));
 
-			for (Card card : repository.findAll()) {
-				System.out.println(card.toString());
-				System.out.println(card.getId());
-				System.out.println(card.getDescription());
+			for (Minion minion : repository.findAll()) {
+				System.out.println(minion.toString());
+				System.out.println(minion.getId());
+				System.out.println(minion.getDescription());
 			}
-			
-			
-
-			/*// fetch an individual customer by ID
-			repository.findById(1L)
-				.ifPresent(customer -> {
-					log.info("Customer found with findById(1L):");
-					log.info("--------------------------------");
-					log.info(customer.toString());
-					log.info("");
-				});
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			// 	log.info(bauer.toString());
-			// }
-			log.info("");*/
 		};
 	}
 }

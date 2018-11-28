da79c1831d32fce6e39024b660f0522a389c4e1f
Martin <ars.martin@hotmail.fr>
2018-11-28 21:05:55 +0100

    Update

--- Engine/src/main/java/fr/univ_nantes/alma/engine/Engine.java
+++ Engine/src/main/java/fr/univ_nantes/alma/engine/Engine.java
@@ -10,8 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import fr.univ_nantes.alma.DAOApplication;
-
 /**
  * 
  * 

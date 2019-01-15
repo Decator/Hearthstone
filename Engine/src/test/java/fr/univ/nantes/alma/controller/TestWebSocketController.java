bd2144421add46e1cd4475105a39211f14ef32fc
Faenya <claveau.alexis@gmail.com>
2019-01-15 11:46:30 +0100

    More testing

---
+++ Engine/src/test/java/fr/univ/nantes/alma/controller/TestWebSocketController.java
@@ -0,0 +1,13 @@
+package fr.univ.nantes.alma.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestWebSocketController {
+
+  @Test
+  void test() {
+  }
+
+}
